26d8c8ea43931e26c7751495961f129cbec27b51
jiangjun <jiangjun@github.io>
2017-12-18 12:12:16 +0800

    修改查询管理界面

--- src/components/ManagerSearch.jsx
+++ src/components/ManagerSearch.jsx
@@ -14,73 +14,73 @@ const option = Select.Option;
           <p className = 'headerManager'>定义单数据源：</p>
           <Button type="primary" icon="plus">添加数据</Button>
           <div className = 'contentManager'>
-            <Row>
-                <Col span={2}>数据类型:</Col>
-                <Col span={4}>数据源:</Col>
-                <Col span={9}>多字段选择:</Col>
-                <Col span={5}>名称:</Col>
+            <Row gutter={16}>
+                <Col span={3} className="gutter-row">数据类型:</Col>
+                <Col span={3} className="gutter-row">数据源:</Col>
+                <Col span={9} className="gutter-row">多字段选择:</Col>
+                <Col span={5} className="gutter-row">名称:</Col>
             </Row>
-            <Row>
-                <Col span={2} >
+            <Row gutter={16}>
+                <Col span={3} className="gutter-row">
                     <Select defaultValue="DB" style={{ width: '100%' }}>
-                        <option value="">DB</option>
-                        <option value="">ORACLE</option>
+                        <option value="" key = ''>DB</option>
+                        <option value="" key = ''>ORACLE</option>
                     </Select>
                 </Col>
-                <Col span={4} >
+                <Col span={3} className="gutter-row">
                     <Select defaultValue="选择index" style={{ width: '100%' }}>
-                        <option value="">选择index</option>
-                        <option value="">选择type</option>
+                        <option value="" key = ''>选择index</option>
+                        <option value="" key = ''>选择type</option>
                     </Select>
                 </Col>
-                <Col span={9} >
+                <Col span={9} className="gutter-row">
                     <Select
                         mode="tags"
                         placeholder="Please select"
                         defaultValue={[]}
                         style={{ width: '100%' }}
                     >
-                        <option value="" >a</option>
-                        <option value="" >b</option>
+                        <option value="" key = ''>a</option>
+                        <option value="" key = ''>b</option>
                     </Select>
                 </Col>
-                <Col span={5} >
+                <Col span={5} className="gutter-row">
                     <Input /> 
                 </Col>
-                <Col span={4} >
+                <Col span={4} className="gutter-row">
                     <Button type="primary">保存</Button>
                 </Col>
             </Row>
           </div> 
           <div className = 'contentManager'>
-          <Row>
-                <Col span={2} >
+          <Row gutter={16}>
+                <Col span={3} className="gutter-row">
                     <Select defaultValue="" style={{ width: '100%' }}>
-                        <option value="">DB</option>
-                        <option value="">ORACLE</option>
+                        <option value="" key = ''>DB</option>
+                        <option value="" key = ''>ORACLE</option>
                     </Select>
                 </Col>
-                <Col span={4} >
+                <Col span={3} className="gutter-row">
                     <Select defaultValue="" style={{ width: '100%' }}>
-                        <option value="">选择index</option>
-                        <option value="">选择type</option>
+                        <option value="" key = ''>选择index</option>
+                        <option value="" key = ''>选择type</option>
                     </Select>
                 </Col>
-                <Col span={9} >
+                <Col span={9} className="gutter-row">
                     <Select
                         mode="tags"
                         placeholder="Please select"
                         defaultValue={[]}
                         style={{ width: '100%' }}
                     >
-                        <option value="" >a</option>
-                        <option value="" >b</option>
+                        <option value="" key = ''>a</option>
+                        <option value="" key = ''>b</option>
                     </Select>
                 </Col>
-                <Col span={5} >
+                <Col span={5} className="gutter-row">
                     <Input /> 
                 </Col>
-                <Col span={4} >
+                <Col span={4} className="gutter-row">
                     <Button>编辑</Button>
                     <Button>删除</Button>
                 </Col>
