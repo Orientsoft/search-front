41c1edb00cac47f02ee57bf1a0aa4d9e5fcb6702
pixcai <pixcai@163.com>
2017-12-19 18:56:21 +0800

    实现搜索查询

--- src/components/SearchPanel.jsx
+++ src/components/SearchPanel.jsx
@@ -12,6 +12,7 @@ import Component from './Component';
 const FormItem = Form.Item;
 const { Search } = Input;
 const { RangePicker } = DatePicker;
+const RadioGroup = Radio.Group;
 
 const formItemLayout = {
     labelCol: {
@@ -36,28 +37,37 @@ const marks = {
 
 export default class SearchPanel extends Component {
 
+  // 想要搜索的数据
+  data = {}
+
   render() {
     return (
       <div>
         <Slider marks={marks} step={1} defaultValue={12} />
         <Form>
           <FormItem {...formItemLayout} label="快速选择">
-            <Radio>1小时</Radio>
-            <Radio>6小时</Radio>
-            <Radio>一天</Radio>
-            <Radio>一周</Radio>
+          <RadioGroup onChange={(e) => this.onTimeChange(e.target.value)}>
+            <Radio value={60}>1小时</Radio>
+            <Radio value={6 * 60}>6小时</Radio>
+            <Radio value={24 * 60}>一天</Radio>
+            <Radio value={7 * 24 * 60}>一周</Radio>
+          </RadioGroup>
           </FormItem>
           <FormItem {...formItemLayout} label="自定义时间">
-            <RangePicker />
+            <RangePicker showTime={{
+              hideDisabledOptions: true
+            }} format="YYYY-MM-DD HH:mm:ss" onChange={(date) => this.onDateChange(date)} />
           </FormItem>
           <FormItem {...formItemLayout} label="过滤字段">
-            <Radio>交易码</Radio>
-            <Radio>耗时</Radio>
-            <Radio>交易内容</Radio>
-            <Radio>非成功交易</Radio>
+            <RadioGroup onChange={(e) => this.onFieldChange(e.target.value)}>
+              <Radio value={'TransCode'}>交易码</Radio>
+              <Radio value={'UseTime'}>耗时</Radio>
+              <Radio value={'TranName'}>交易内容</Radio>
+              <Radio value={'TranCode'}>非成功交易</Radio>
+            </RadioGroup>
           </FormItem>
           <FormItem {...formItemLayout} label="过滤条件">
-            <Search placeholder="Search..." enterButton />
+            <Search placeholder="Search..." enterButton onSearch={(value) => this.onSearch(value)} />
           </FormItem>
           <FormItem {...formItemLayout} label="查询条件">
             <Button type="primary">保存查询条件</Button>
@@ -65,5 +75,58 @@ export default class SearchPanel extends Component {
         </Form>
       </div>
     )
+  }
+
+  onTimeChange(value) {
+
+  }
+
+  onDateChange(date) {
+      this.data.datetime = {
+        from: date[0].toJSON(),
+        to: date[1].toJSON()
+      };
+  }
+
+  onFieldChange(field) {
+    this.data.field = field;
+  }
+
+  onSearch(value) {
+    const fieldValue = value;
+    console.log(this.data, fieldValue)
+    const { datetime, field } = this.data;
+
+    this.elastic.search({
+      index: 'tpload-*',
+      body: {
+        aggs: {
+          [field]: {
+            date_histogram: {
+              field: '@TranTime',
+              interval: 'month'
+            }
+          }
+        },
+        query: {
+          bool: {
+            filter: [{
+              range: {
+                '@TranTime': {
+                  from: datetime.from,
+                  to: datetime.to
+                }
+              }
+            }, {
+              match_phrase: {
+                [field]: {
+                  query: fieldValue
+                }
+              }
+            }]
+          }
+        }
+      }
+    });
   }
 }
