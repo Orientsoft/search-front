51e7fd8f255367901892354a211d14c956251cc3
pixcai <pixcai@163.com>
2017-12-19 12:09:56 +0800

    Rename es.js to elastic.js

--- src/index.jsx
+++ src/index.jsx
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import App from './components/App';
 import stores from './stores';
+import elastic from './services/elastic';
 import './less/styles.less';
 
 // wrap App in AppContainer to support react hot loader
 const render = () => {
     ReactDOM.render(
         <AppContainer>
-            <App store={stores} />
+            <App store={stores} elastic={elastic} />
         </AppContainer>,
         document.getElementById('mobx_container')
     );
