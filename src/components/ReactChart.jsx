914e1976b038f23f3019295b7fcb4ec24029f2ad
pixcai <pixcai@163.com>
2018-01-09 14:30:56 +0800

    更新图表

--- src/components/ReactChart.jsx
+++ src/components/ReactChart.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import BaseComponent from './BaseComponent';
 import { observer } from 'mobx-react';
 import { computed } from 'mobx';
+import get from 'lodash/get';
 import * as moment from 'moment';
-
 
 const chartConfig = [
     { name: '手机', fileds: {}, type: 'line', title: '中间件', Yaxis: '', Xaxis: '', Ytitle: '数量(万笔)', Xtitle: '时间' },
     { name: '手机', fileds: {}, type: 'histogram', title: 'tploader', Yaxis: '', Xaxis: '', Ytitle: '数量(万笔)', Xtitle: '时间' }
-]
+];
+
 @observer class ReactChart extends BaseComponent {
     constructor(props, context) {
         super(props, context);
@@ -21,41 +22,41 @@ const chartConfig = [
         this.darkenColor = this.darkenColor.bind(this);
     }
 
-
     @computed get data() {
-        const aggs = this.getBuckets(this.queryStore.filterFields[0].field);
+        const buckets = this.getBuckets('@timestamp');
+        const result = this.queryStore.filterFields.map(() => []);
 
-        return aggs.map(agg => this.getBuckets('@timestamp', {
-            aggregations: agg
-        })).reduce((result, buckets) => result.concat(buckets.map(bucket => [
-            new Date(bucket.key),
-            bucket.doc_count
-        ])), []);
+        return buckets.reduce((result, bucket) => {
+            this.queryStore.filterFields.forEach((field, key) => {
+                result[key].push([
+                    new Date(bucket.key),
+                    get(bucket[field.field], 'value', 0)
+                ])
+            });
+            return result;
+        }, result);
     }
-    
 
     render() {
         return (
             <div className="clearfix">
-               
-                {
-                    this.state.chartConfig.length > 0 && this.data.length > 0 && this.state.chartConfig.map((item, key) => (
-                        <div key={key} className="chart">
-                            <div key={key} ref={el => this.initChart(el, key, this.data, item)}></div>
-                        </div>
-                    ))
-                }
+                {this.state.chartConfig.length > 0 && this.data.length > 0 && this.state.chartConfig.map((item, key) => (
+                    <div key={key} className="chart">
+                        <div key={key} ref={el => this.initChart(el, key, this.data[key], item)}></div>
+                    </div>
+                ))}
             </div>
         );
     }
+
     componentDidUpdate() {
         if (this.charts.length > 0 && this.charts.length == this.state.chartConfig.length) {
-            var sync = Dygraph.synchronize(this.charts);
+            const sync = Dygraph.synchronize(this.charts);
         }
     }
+
     initChart(el, key, data, chartConfig) {
-        console.log('进入initChart');
-        var chart = this.charts[key];
+        let chart = this.charts[key];
         if (!chart) {
             if (chartConfig.type == 'histogram') {
                 chart = new Dygraph(el, data, {
@@ -79,7 +80,6 @@ const chartConfig = [
                         })
                     ]
                 })
-
             } else if (chartConfig.type == 'line') {
                 chart = new Dygraph(el, data, {
                     labels: ['x', '数量'],
@@ -109,9 +109,10 @@ const chartConfig = [
             this.charts[key] = chart
         }
     }
+
     darkenColor(colorStr) {
         // Defined in dygraph-utils.js
-        var color = Dygraph.toRGB_(colorStr);
+        const color = Dygraph.toRGB_(colorStr);
         color.r = Math.floor((255 + color.r) / 2);
         color.g = Math.floor((255 + color.g) / 2);
         color.b = Math.floor((255 + color.b) / 2);
@@ -119,25 +120,25 @@ const chartConfig = [
     }
 
     barChartPlotter(e) {
-        var ctx = e.drawingContext;
-        var points = e.points;
-        var y_bottom = e.dygraph.toDomYCoord(0);
+        let ctx = e.drawingContext;
+        let points = e.points;
+        let y_bottom = e.dygraph.toDomYCoord(0);
 
         ctx.fillStyle = this.darkenColor(e.color);
 
         // Find the minimum separation between x-values.
         // This determines the bar width.
-        var min_sep = Infinity;
-        for (var i = 1; i < points.length; i++) {
-            var sep = points[i].canvasx - points[i - 1].canvasx;
+        let min_sep = Infinity;
+        for (let i = 1; i < points.length; i++) {
+            let sep = points[i].canvasx - points[i - 1].canvasx;
             if (sep < min_sep) min_sep = sep;
         }
-        var bar_width = Math.floor(2.0 / 3 * min_sep);
+        let bar_width = Math.floor(2.0 / 3 * min_sep);
 
         // Do the actual plotting.
-        for (var i = 0; i < points.length; i++) {
-            var p = points[i];
-            var center_x = p.canvasx;
+        for (let i = 0; i < points.length; i++) {
+            let p = points[i];
+            let center_x = p.canvasx;
 
             ctx.fillRect(center_x - bar_width / 2, p.canvasy,
                 bar_width, y_bottom - p.canvasy);
@@ -157,9 +158,7 @@ const chartConfig = [
                 chartConfig: chartConfig
             })
         }
-
     }
-
 }
 
 export default ReactChart;
